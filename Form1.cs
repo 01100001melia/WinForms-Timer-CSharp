4189c32e1fcbf6325becbfc0576de97815567219
Amelia <157375868+01100001melia@users.noreply.github.com>
2026-03-20 18:06:40 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Timer_App
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int Counter = 0;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Counter++;
+            lblTime.Text = Counter.ToString();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+        }
+
+        private void btnPause_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            Counter = 0;
+            lblTime.Text = "0";
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            Counter = 0;
+            lblTime.Text = "0";
+        }
+    }
+}
